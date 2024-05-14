faaee9b2b7b4c020efdab760459352a2e73b5de2
Harvmaster <hzuccon@gmail.com>
2024-05-15 00:07:26 +1000

    Moved PortableModel validator to utils

--- src/services/engines/EngineManager.ts
+++ src/services/engines/EngineManager.ts
@@ -1,4 +1,6 @@
 import { OpenAI, Ollama } from ".";
+import { validatePortableModel } from "./utils";
+
 import { ClosedEngineProps, Engine, EngineProps, Model, OpenEngineProps, PortableModel } from "./types";
 
 export class EngineManager {
@@ -33,7 +35,7 @@ export class EngineManager {
     return engine
   }
 
-   importModel (model: PortableModel) {
+  importModel (model: PortableModel) {
     validatePortableModel(model)
 
     let engine = this.getOrCreateEngine(model)
@@ -43,37 +45,6 @@ export class EngineManager {
 
     const newModel = engine.createModel(model)
     this.models.push(newModel)
-  }
-}
-
-const validatePortableModel = (model: PortableModel) => {
-  if (!model.id) {
-    throw new Error('Model must have an id')
-  }
-  if (!model.name) {
-    throw new Error('Model must have a name')
-  }
-  if (!model.model) {
-    throw new Error('Model must have a model')
-  }
-  if (!model.engine) {
-    throw new Error('Model must have an engine')
-  }
-
-  if (!model.engine.id) {
-    throw new Error('Model engine must have an id')
-  }
-
-  if (!model.engine.name) {
-    throw new Error('Model engine must have a name')
-  }
-
-  if (!model.engine.type) {
-    throw new Error('Model engine must have a type')
-  }
-
-  if (!model.engine.createdAt) {
-    throw new Error('Model engine must have a createdAt')
   }
 }
 
