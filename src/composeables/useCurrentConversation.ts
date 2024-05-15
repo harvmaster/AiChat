f579caa57658ee66aeb5fcd125384abf138bfb61
Harvmaster <hzuccon@gmail.com>
2024-05-15 11:19:22 +1000

    useCurrentConversation is now a singleton

--- src/composeables/useCurrentConversation.ts
+++ src/composeables/useCurrentConversation.ts
@@ -1,16 +1,33 @@
 import { useRouter } from 'vue-router'
 import { app } from 'boot/app'
 import Conversation from 'src/utils/App/Conversation'
-import { ComputedRef, computed } from 'vue'
 
-export const useCurrentConversation = (): ComputedRef<Conversation | undefined> => {
+import { Ref, WatchStopHandle, ref, watch } from 'vue'
+// import { ComputedRef, Ref, WatchStopHandle, computed, ref, watch } from 'vue'
+
+// export const useCurrentConversation = (): ComputedRef<Conversation | undefined> => {
+//   const router = useRouter()
+//   const conversationId = computed(() => router.currentRoute.value.params.id as string)
+
+//   const conversation = computed(() => {
+//     return app.conversations.value.find(conversation => conversation.id == conversationId.value)
+//   })
+//   return conversation
+// }
+
+// Multiple components will need this information and it is not dependent on the component itself, so we just use a single instance of the watcher and currentConversation
+const currentConvsation = ref<Conversation | undefined>(undefined)
+let watcher: WatchStopHandle | undefined
+export const useCurrentConversation = (): Ref<Conversation | undefined> => {
   const router = useRouter()
-  const conversationId = computed(() => router.currentRoute.value.params.id as string)
+  
+  if (!watcher) {
+    watcher = watch(() => router.currentRoute.value.params.id, (id) => {
+      currentConvsation.value = app.conversations.value.find(conversation => conversation.id == id)
+    })
+  }
 
-  const conversation = computed(() => {
-    return app.conversations.value.find(conversation => conversation.id == conversationId.value)
-  })
-  return conversation
+  return currentConvsation
 }
 
 export default useCurrentConversation 
